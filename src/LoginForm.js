99989a4bd572dea8f43d1033d910715d1c15f5ff
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-10 21:35:41 +0300

    fixes

---
+++ src/LoginForm.js
@@ -0,0 +1,14 @@
+
+function LoginForm(){
+return (
+<form className="submit">
+           <label className="label">Name</label>
+           <input className="input"></input>
+           <label className="label">Password</label>
+           <input className="input"></input>
+           <button type="submit">Submit</button>
+          </form>
+)
+}
+
+export default LoginForm;
